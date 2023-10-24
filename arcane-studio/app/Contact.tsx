5a1f3341c2090e6f72ebfaa048a1b70166094c87
mariech5071 <125594493+mariech5071@users.noreply.github.com>
2023-10-24 17:09:39 +0200

    Contact form backbone created
    The backbone of the contact from was created.

--- arcane-studio/app/Contact.tsx
+++ arcane-studio/app/Contact.tsx
@@ -4,8 +4,33 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <div id='contact'>Contact</div>
-      <form></form>
+      <div className='text-center pt-5 text-3xl font-bold' id='contact'>Contact</div>
+      <form>
+        <div className='w-full flex flex-col'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' id='name'></input>
+        </div>
+        <div className='w-full flex flex-col'>
+          <label htmlFor='email'>Email</label>
+          <input type='email' id='email'></input>
+        </div>
+        <div className='w-full flex flex-col'>
+          <label htmlFor='organization'>Organization</label>
+          <input type='text' id='organization'></input>
+        </div>
+        <div className='w-full flex flex-col'>
+          <label htmlFor='job-title'>Job Title</label>
+          <input type='text' id='job-title'></input>
+        </div>
+        <div className='w-full flex flex-col'>
+          <label htmlFor='country'>Country</label>
+          <input type='text' id='country'></input>
+        </div>
+        <div className='w-full flex flex-col'>
+          <label htmlFor='inquiry'>Brief description of your inquiry</label>
+          <input type='text' id='inquiry'></input>
+        </div>
+      </form>
     </div>
   )
 };
