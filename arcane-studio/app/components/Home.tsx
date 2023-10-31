7ff271f6b821efbc17a0a0a8f1c617f6fdf9c9b3
mariech5071 <125594493+mariech5071@users.noreply.github.com>
2023-10-31 21:07:48 +0100

    Home image updated

--- arcane-studio/app/components/Home.tsx
+++ arcane-studio/app/components/Home.tsx
@@ -3,8 +3,7 @@ import React from "react";
 //This is the home component, when home is clicked it will bring you to the top of the website
 const Home = () => {
   return (
-  <div className="md:bg-[url('/images/Home.jpg')] bg-[url('/images/Home2.png')] bg-cover pt-20" id='home'>
-    <div className="h-96 w-full">
+  <div className="md:bg-[url('/images/Home.jpg')] bg-[url('/images/Home2.png')] w-full h-60 md:h-full bg-cover pt-20" id='home'>
       <br></br>
       <h1 className='text-white font-semibold px-8 pt-8 text-6xl md:visible invisible' >Virtual Reality</h1>
       <p className='text-white text-lg px-8 pt-4 pb-10 md:visible invisible'>create your own experience</p>
@@ -18,7 +17,6 @@ const Home = () => {
           <p className='text-white p-4 md:visible invisible'>Lorem ipsum dolor sit amet, dolore magna aliqua. Massa tincidunt dui ut ornare.</p>
         </div>
       </div>
-    </div>
   </div>
   )
 };
