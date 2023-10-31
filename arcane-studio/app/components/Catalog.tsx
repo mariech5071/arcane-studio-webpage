d1fd5ffeefe89e26e819ebcfb998b63c0c5a9e14
mariech5071 <125594493+mariech5071@users.noreply.github.com>
2023-10-31 18:59:33 +0100

    Images updated

--- arcane-studio/app/components/Catalog.tsx
+++ arcane-studio/app/components/Catalog.tsx
@@ -11,22 +11,22 @@ const Catalog = () => {
         <div className='text-center text-4xl font-bold mb-4'>Discover our catalog</div>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-3'>
           <div>
-            <img src='/images/Project1.png' className='w-3/4 p-3'></img>
+            <img src='/images/Ima-01.png' className='w-3/4 p-3'></img>
             <h2 className='font-bold'>Product 1</h2>
             <p>in est ante in nibh mauris</p>
           </div>
           <div>
-            <img src='/images/Project2.png' className='w-3/4 p-3'></img>
+            <img src='/images/Ima-02.png' className='w-3/4 p-3'></img>
             <h2 className='font-bold'>Product 2</h2>
             <p>in est ante in nibh mauris</p>
           </div>
           <div>
-            <img src='/images/Project3.png' className='w-3/4 p-3'></img>
+            <img src='/images/Ima-03.png' className='w-3/4 p-3'></img>
             <h2 className='font-bold'>Product 3</h2>
             <p>in est ante in nibh mauris</p>
           </div>
           <div>
-            <img src='/images/Project4.png' className='w-3/4 p-3'></img>
+            <img src='/images/Ima-04.png' className='w-3/4 p-3'></img>
             <h2 className='font-bold'>Product 4</h2>
             <p>in est ante in nibh mauris</p>
           </div>
